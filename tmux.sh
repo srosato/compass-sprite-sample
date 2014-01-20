49474713a6adb6efd0a61cd1243bd987d6c90b10
Steven Rosato <steven.rosato@majisti.com>
2014-01-20 11:26:22 -0500

    Font awesome example.

--- tmux.sh
+++ tmux.sh
@@ -4,7 +4,7 @@ tmux new -s compass-sample -d
 tmux split-window -h
 tmux split-window -h
 
-tmux send -t compass-sample.2 'compass watch -c ./config.rb' ENTER
+tmux send -t compass-sample.2 'compass watch -c sass/config.rb' ENTER
 
 tmux select-pane -t 0
 tmux select-layout 6a99,237x57,0,0{158x57,0,0,78x57,159,0[78x28,159,0,78x28,159,29]}
